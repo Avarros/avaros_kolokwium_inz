930d2a041e351bb8505d60bd43aab457bb42de3c
Avarros <60660200+Avarros@users.noreply.github.com>
2023-01-17 18:52:41 +0100

    Add files via upload

---
+++ ObslugaBazyZEntityFramework.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ObslugaBazyZEntityFramework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    #region obslugaBazyDanych
+    class BazaDanychObsluga : DbContext
+    {
+        public DbSet<pomiar /*<--nazwa klasy*/> pomiar/*<--dowolna logiczna nazwa*/ { get; set; }
+        public DbSet<sensor> sensor { get; set; }
+        //nic na dole nie zmieniac oprocz nazwy bazy
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            dbContextOptionsBuilder.UseSqlite("Data Source=pomiary.db");
+            base.OnConfiguring(dbContextOptionsBuilder);
+        }
+    }
+    #endregion
+    #region opis_tabeli_przyklad
+    class pomiar
+    {
+        //kolumny, te same typy danych musza byc
+        public int pomiarId { get; set; }
+        public string wartosc1 { get; set; }
+        public string wartosc2 { get; set; }
+        public string wartosc3 { get; set; }
+        public int sensorid { get; set; }
+    }
+    #endregion
+    #region zapis_danych
+    private void buttonZapiszDb_Click(object sender, EventArgs e)
+    {
+        using (var strukturaDanych = new BazaDanychObsluga())
+        {
+            strukturaDanych.pomiar.Add(new pomiar() { wartosc1 = "123", wartosc2 = "98", wartosc3 = "101" });
+            strukturaDanych.SaveChanges();
+        }
+    }
+    #endregion
+    #region odswiez_grid_view
+    private void buttonOdswiez_Click(object sender, EventArgs e)
+    {
+        BazaDanychObsluga bazaDanychObsluga = new BazaDanychObsluga();
+        var DaneWczytane = from BazaDanychObsluga in bazaDanychObsluga.pomiar select BazaDanychObsluga;
+        dataGridViewPomiary.Rows.Clear();
+        dataGridViewPomiary.Columns.Clear();
+        dataGridViewPomiary.Columns.Add("pomiarId", "Identyfikator");
+        dataGridViewPomiary.Columns.Add("wartosc1", "Pomiar1");
+        dataGridViewPomiary.Columns.Add("wartosc2", "Pomiar2");
+        dataGridViewPomiary.Columns.Add("wartosc3", "Pomiar3");
+
+        foreach (var kursor in DaneWczytane)
+        {
+            dataGridViewPomiary.Rows.Add(kursor.pomiarId, kursor.wartosc1, kursor.wartosc2, kursor.wartosc3);
+        }
+    }
+    #endregion
+    #region aktualizuj_dane
+    private void buttonAktualizuje_Click(object sender, EventArgs e)
+    {
+        using (var daneAktualizacja = new BazaDanychObsluga())
+        {
+            int idSensor = Int32.Parse(textBoxIdPomiar.Text.ToString());
+            pomiar _pomiar = new pomiar() { pomiarId = idSensor, wartosc1 = textBoxWartosc1.Text.ToString() };
+            daneAktualizacja.Entry(_pomiar).State = EntityState.Modified;
+            daneAktualizacja.SaveChanges();
+        }
+    }
+    #endregion
+    #region usun_dane
+    private void buttonUsun_Click(object sender, EventArgs e)
+    {
+        using (var daneAktualizacja = new BazaDanychObsluga())
+        {
+            int idSensor = Int32.Parse(textBoxIdPomiar.Text.ToString());
+            pomiar _pomiar = new pomiar() { pomiarId = idSensor };
+            daneAktualizacja.Entry(_pomiar).State = EntityState.Deleted;
+            daneAktualizacja.SaveChanges();
+        }
+    }
+    #endregion
+    #region cell_click_grid_view
+    private void dataGridViewPomiary_CellClick(object sender, DataGridViewCellEventArgs e)
+    {
+        string wybraneIdPomia = dataGridViewPomiary.CurrentCell.Value.ToString();
+        textBoxIdPomiar.Text = wybraneIdPomia.ToString();
+    }
+    #endregion
+}
---
+++ ObslugaBazyZSQLite.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ObslugaBazyDanychZSqlite
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+    #region opis_bazy_danych
+    class BazaDanychOpis
+    {
+
+        public static string BAZADANYCH_TABELA_wartosciCyfrowe = "wartosciCyfrowe"; //nazwa tabeli
+        public static string WARTOSCI_CRYFROWE_IdwartosciCyfrowe = "idwartosciCyfrowe"; //nazwa kolumny
+        public static string WARTOSCI_CRYFROWEK_wartosci = "wartosc"; //nazwa kolumny
+    }
+    #endregion
+    #region polaczenie_z_baza_danych
+    class BazaLaczkaRozlaczka
+    {
+        public static SQLiteConnection sql_con;
+        private SQLiteCommand sql_cmd;
+        public static string BAZA_DANYCH_KSIAZKA_NAD_PLIK = "pomiary.db"; //pomiary.db to nazwa bazy, wymien ja na inna
+        public static string BAZA_DANYCH_KSIAZKA_NAD_LACZKA = "Data Source= " + BAZA_DANYCH_KSIAZKA_NAD_PLIK + ";Version=3;New=False;Compress=True;"; //to zostaw w spokoju
+
+        public SQLiteCommand Polacz()
+        {
+            try
+            {
+                sql_con = new SQLiteConnection(BAZA_DANYCH_KSIAZKA_NAD_LACZKA);
+                sql_con.Open();
+                sql_cmd = sql_con.CreateCommand();
+            }
+            catch
+            {
+                MessageBox.Show("Nie mozna polaczyc sie z baza danych");
+            }
+            return sql_cmd;
+        }
+
+        public void Rozlacz()
+        {
+            try
+            {
+                sql_con.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Nie mozna polaczyc sie z baza danych");
+            }
+        }
+    }
+    #endregion
+    #region insertSQL
+    class InsertSql
+    {
+        public string dodajRekord_1_pole(string nazwaTabeli, string nazwaPola, string wartoscPola)
+        {
+            string sqlString = null;
+
+            sqlString = "INSERT INTO " + nazwaTabeli + " (" + nazwaPola + ") VALUES ('" + wartoscPola + "');";
+
+            return sqlString;
+        }
+    }
+    private void textBoxWartoscDoZapisu_TextChanged(object sender, EventArgs e)
+    {
+        if (checkBoxZapis.Checked == true)
+        {
+            BazaLaczkaRozlaczka bazaLaczkaRozlaczka = new BazaLaczkaRozlaczka();
+            InsertSql insertSql = new InsertSql();
+            SQLiteCommand sql_cmd = bazaLaczkaRozlaczka.Polacz();
+
+            sql_cmd.CommandText = insertSql.dodajRekord_1_pole(BazaDanychOpis.BAZADANYCH_TABELA_wartosciCyfrowe, BazaDanychOpis.WARTOSCI_CRYFROWEK_wartosci, textBoxWartoscDoZapisu.Text);
+            SQLiteDataReader kursor1 = sql_cmd.ExecuteReader();
+            bazaLaczkaRozlaczka.Rozlacz();
+        }
+    }
+    #endregion
+    #region selectSQL
+    class BazaDanychSqlSelect
+    {
+        public string wczytajTabeleCala(string nazwaTabeli)
+        {
+            string sqlString = null;
+
+            sqlString = "SELECT * FROM " + nazwaTabeli + ";";
+
+            return sqlString;
+        }
+
+        public string wczytaj_1_Pole(string nazwaTabeli, string nazwaPola)
+        {
+            string sqlString = null;
+
+            sqlString = "SELECT " + nazwaPola + " FROM " + nazwaTabeli + ";";
+
+            return sqlString;
+        }
+        public string wczytaj_2_Pole(string nazwaTabeli, string nazwaPola1, string nazwaPola2)
+        {
+            string sqlString = null;
+
+            sqlString = "SELECT " + nazwaPola1 + ", " + nazwaPola2 + " FROM " + nazwaTabeli + ";";
+
+            return sqlString;
+        }
+
+    }
+    private void buttonWczytajTestdane_Click(object sender, EventArgs e)
+    {
+        BazaLaczkaRozlaczka bazaLaczkaRozlaczka = new BazaLaczkaRozlaczka();
+        BazaDanychSqlSelect bazaDanychSqlSelect = new BazaDanychSqlSelect();
+        SQLiteCommand sql_cmd = bazaLaczkaRozlaczka.Polacz();
+        string nazwaMiasta = "";
+        sql_cmd.CommandText = bazaDanychSqlSelect.wczytaj_1_Pole(BazaDanychOpis.BAZADANYCH_TABELA_MIASTA_SLOWNIK, BazaDanychOpis.MIASTA_SLOWNIK_NAZWA_MIASTA);
+
+        SQLiteDataReader kursor1 = sql_cmd.ExecuteReader();
+        while (kursor1.Read())
+        {
+            nazwaMiasta = kursor1.GetString(0);
+        }
+
+        bazaLaczkaRozlaczka.Rozlacz();
+        MessageBox.Show(nazwaMiasta);
+
+    }
+    #endregion
+}
