8d8e237ba0c8d4ac217362be8ce2a7fcd379b1bb
Avarros <60660200+Avarros@users.noreply.github.com>
2023-01-17 20:53:56 +0100

    Add files via upload

---
+++ Pad/Form1.cs
@@ -0,0 +1,66 @@
+﻿using SharpDX.DirectInput;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PadPadPad
+{
+    public partial class Form1 : Form
+    {
+        PadKierownica padKierownica = new PadKierownica();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void manipulatorStart()
+        {
+            Joystick joystick = padKierownica.wyszukajManipulator();
+            padKierownica.obslugaZdarzenManipulatora(joystick);
+        }
+
+        private void buttonPolacz_Click(object sender, EventArgs e)
+        {
+            timerJoy.Enabled = true;
+            Joystick joystick = padKierownica.wyszukajManipulator();
+            textBox1.Text = PadKierownica.joystickGuid.ToString();
+            Thread thread = new Thread(manipulatorStart);
+            thread.Start();
+        }
+
+        private void timerJoy_Tick(object sender, EventArgs e)
+        {
+            textBoxManipulatora.Text = PadKierownica.wskazanieManipulatora;
+        }
+
+        void ustawLicznik()
+        {
+            int gdzie_ = textBoxManipulatora.Text.IndexOf("_");
+            int dlugosc = textBoxManipulatora.Text.Length;
+            string przycisk = textBoxManipulatora.Text.Remove(gdzie_);
+            string wartosc = textBoxManipulatora.Text.Replace(przycisk + "_", "");
+
+            if (przycisk == "X")
+            {
+                lbAnalogMeter1.Value = int.Parse(wartosc);
+            }
+        }
+
+        private void textBoxManipulatora_TextChanged(object sender, EventArgs e)
+        {
+            int gdzie_ = textBoxManipulatora.Text.IndexOf("_");
+            int dlugosc = textBoxManipulatora.Text.Length;
+            string przycisk = textBoxManipulatora.Text.Remove(gdzie_);
+            string wartosc = textBoxManipulatora.Text.Replace(przycisk + "_", "");
+            textBoxA.Text = przycisk;
+            textBoxB.Text = wartosc;
+            ustawLicznik();
+        }
+    }
+}
---
+++ Pad/PadKierownica.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX.DirectInput;
+
+namespace PadPadPad
+{
+    class PadKierownica
+    {
+        public static string wskazanieManipulatora = "";
+        public static Guid joystickGuid = Guid.Empty;
+
+
+        public Joystick wyszukajManipulator()
+        {
+            string czyWyszukanoManipulator = "T";
+            try
+            {
+                var directInput = new DirectInput();
+
+                foreach (var deviceInstance in directInput.GetDevices(DeviceType.Gamepad, DeviceEnumerationFlags.AllDevices))
+                    joystickGuid = deviceInstance.InstanceGuid;
+                if (joystickGuid == Guid.Empty)
+                    foreach (var deviceInstance in directInput.GetDevices(DeviceType.Joystick, DeviceEnumerationFlags.AllDevices))
+                        joystickGuid = deviceInstance.InstanceGuid;
+                if (joystickGuid == Guid.Empty)
+                {
+                    czyWyszukanoManipulator = "N";
+                }
+
+                //dodatkowe info o rodzaju manipulatora
+                var joystick = new Joystick(directInput, joystickGuid);
+                var allEffects = joystick.GetEffects();
+                return joystick;
+
+            }
+            catch
+            {
+                return null;
+            };
+        }
+
+        public void obslugaZdarzenManipulatora(Joystick joy)//dla konsoli
+        {
+
+            joy.Properties.BufferSize = 128;
+
+            joy.Acquire();
+
+            while (true)
+            {
+
+                joy.Poll();
+                var datas = joy.GetBufferedData();
+                foreach (var state in datas)
+
+                    wskazanieManipulatora = (state.Offset + "_" + state.Value);
+
+            }
+
+        }
+    }
+}
